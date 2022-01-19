89472d08c913400a9fbfd15eb10b422d0fdd8132
VegetoMika <mikarue@yahoo.fr>
2022-01-19 15:35:41 +0100

    new branch commit

--- includes/Ajax_API.php
+++ includes/Ajax_API.php
@@ -1,37 +1,31 @@
 <?php
 
-class Ajax_API extends API_Config {
+require __DIR__ . '/TokenController.php';
 
+class Ajax_API extends API_Config {
+    
     public function __construct()
     {
-        if (!empty($_GET)) {
-
-            if (isset($_GET['token']) && $_GET['token'] == '1234') {
-
-                echo $this->filterRequest($_GET);
-
-            } else {
-
-                echo json_encode(['data' => null, 'error' => 'Bad token']);
-            }
-        }
+        TokenController::inspectToken($_GET, function($data){
+            echo $this->filterRequest($data);
+        });
     }
-
+    
     private function filterRequest($data)
     {
         if (isset($data['table']) && ($data['table'] === 'articles' || $data['table'] === 'users')) {
-
+            
             if (isset($data['data'])) {
-
+                
                 return json_encode(['data' => $this->getOneFrom($data['table'], $data['data']), 'error' => null]);
-
+                
             } else {
-
+                
                 return json_encode(['data' => $this->getAllTable($data['table']), 'error' => null]);
             }
-
+            
         } else {
-
+            
             return json_encode(['data' => null, 'error' => 'Bad table']);
         }
     }
